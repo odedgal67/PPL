a8d112a84d18db5fc9b9d8d2aba4c45c2d3a8c9e
odedgal67 <62900689+odedgal67@users.noreply.github.com>
2021-06-04 20:51:19 +0300

    finished up to 2.3

--- ex4/part2/src/part2.ts
+++ ex4/part2/src/part2.ts
@@ -9,46 +9,155 @@ type PromisedStore<K, V> = {
 }
 
 
-// export function makePromisedStore<K, V>(): PromisedStore<K, V> {
-//     ???
-//     return {
-//         get(key: K) {
-//             ???
-//         },
-//         set(key: K, value: V) {
-//             ???
-//         },
-//         delete(key: K) {
-//             ???
-//         },
-//     }
-// }
+export function makePromisedStore<K, V>(): PromisedStore<K, V> {
+    let map : Map<K,V> = new Map<K,V>();
+    return {
+        get(key: K){ 
+            return new Promise<V> ((resolve, reject) => {
+                const returnValue = map.get(key)
+                returnValue !== undefined ?
+                resolve(returnValue) : reject(MISSING_KEY)
+            });
+        },
+        set(key: K, value: V)
+        {
+            return new Promise<void> ((resolve) => {
+                map.set(key, value);
+                resolve();
+            })
+        },
+        delete(key: K) 
+        {
+            return new Promise<void>((resolve, reject)=>
+            {
+                map.delete(key) ? resolve()
+                :reject(MISSING_KEY)
+            }
+            )
+        }
+    }
+}
 
-// export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): ??? {
-//     ???
-// }
 
-/* 2.2 */
 
-// ??? (you may want to add helper functions here)
-//
-// export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
-//     ???
-// }
 
-/* 2.3 */
+ export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): Promise<V[]> {
+    let arr : V[] = [];
+    return new Promise((resolve, reject)=>{
+        keys.map((key:K) =>
+        {
+            store.get(key).then((v)=>arr.push(v)).catch(()=>reject(MISSING_KEY));
+        }
+        )
+        resolve(arr);
+    })
 
-// export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: ???): ??? {
-//     ???
-// }
+ }
 
-// export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: ???): ??? {
-//     ???
-// }
+ /* 2.2 */
 
-/* 2.4 */
-// you can use 'any' in this question
+ async function asyncMemoHelper<T,R>(store: PromisedStore<T,R>, f: (param :T)=>R, param:T) : Promise<R>
+ {
+    try
+    {
+      return await store.get(param);
+    }
+    catch
+    {
+        store.set(param,f(param));
+        return await store.get(param);
+    }
+    
+ }
 
-// export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...(???)[]]): Promise<any> {
-//     ???
-// }
+ export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
+    let store : PromisedStore<T,R> = makePromisedStore();
+
+    return (param:T)=>asyncMemoHelper(store, f, param)
+ }
+
+// /* 2.3 */
+
+ export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: (param : T) => Boolean): ()=> Generator<T> {
+    function* newGen()
+    {
+        const iterator = genFn();
+        for (let x of iterator)
+        {
+            if (filterFn(x))
+            {
+                yield(x);
+            }
+        }
+    }
+    return newGen;
+ }
+
+ export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: (param:T) => R): () => Generator<R> {
+    function* newGen()
+    {
+        const iterator = genFn();
+        for(let x of iterator)
+        {
+            yield(mapFn(x));
+        }
+    } 
+    return newGen;
+
+ }
+
+// /* 2.4 */
+
+async function asyncWaterfallWithRetryHelper2(numOfFails:number, func : (() => Promise<any>), reject:()=>any)
+{
+    let returnValue:any;
+    try
+    {
+        returnValue =  await func()
+    }
+    catch
+    {
+        if (numOfFails<2)
+        {
+            new Promise<any> ((resolve, reject)=>{
+                setTimeout(()=>{asyncWaterfallWithRetryHelper(numOfFails+1, func, reject)}, 2000)
+            })  
+        }
+        else{
+            reject()
+        }
+        
+    }
+}
+
+async function asyncWaterfallWithRetryHelper(numOfFails:number, func : ((param:any) => Promise<any>), reject:()=>any)
+{
+    let returnValue:any;
+    try
+    {
+        returnValue =  await func(returnValue)
+    }
+    catch
+    {
+        if (numOfFails<2)
+        {
+            new Promise<any> ((resolve, reject)=>{
+                setTimeout(()=>{asyncWaterfallWithRetryHelper(numOfFails+1, func, reject)}, 2000)
+            })  
+        }
+        else{
+            reject()
+        }
+        
+    }
+}
+ export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((param:any) => Promise<any>)[]]): Promise<any> {
+    let numOfFailsFirst:number=0;
+    let returnValue:any;
+    asyncWaterfallWithRetryHelper2(0,fns[0],()=>console.log("harahara"))
+    fns.slice(1).map(async (func : ((param:any) => Promise<any>))=>{
+        asyncWaterfallWithRetryHelper(0, func, ()=>console.log("hara"))
+   
+    })
+    console.log(returnValue);
+ }
